b10af7fde2b1e680a47f6ad57b359b2fbfcd3617
BrainDelaiy / Philipp Sonntag <sonntag.philipp@outlook.de>
2017-08-24 18:40:01 +0200

    Config controller works

--- PenAndPaperREST/PNPService.Client.Windows/Program.cs
+++ PenAndPaperREST/PNPService.Client.Windows/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PNPService.Shared.Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- PenAndPaperREST/PNPService.Shared.Controller/ConfigController.cs
+++ PenAndPaperREST/PNPService.Shared.Controller/ConfigController.cs
@@ -4,26 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PNPService.Shared.Models.Contracts;
 using Newtonsoft.Json;
 using System.IO;
+using PNPService.Shared.Models;
+using PNPService.Shared.Models.Contracts;
 
 namespace PNPService.Shared.Controller
 {
     public class ConfigController : IConfigController
     {
-        private IConfig _Config;
-
-        public  IConfig Config {
-            get { return _Config; }
+        /// <summary>
+        /// The object that stores all the config data.
+        /// </summary>
+        public IConfig Config {
+            get { return _Config == null ? _Config = new Config() : _Config; }
             set { _Config = value; }
         }
+        private IConfig _Config;
+        
 
+
+        /// <summary>
+        /// Reads the content of the ServerConfig.json and puts it into the config object.
+        /// </summary>
         public void ReadFile()
         {
-            _Config = JsonConvert.DeserializeObject<IConfig>(File.ReadAllText(@".\ServerConfig.json"));
+            Config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(@".\ServerConfig.json"));
         }
 
+
+        /// <summary>
+        /// Writes the content of the ServerConfig.json to a file called "ServerConfig.json".
+        /// </summary>
         public void WriteFile()
         {
             File.WriteAllText(@".\ServerConfig.json", JsonConvert.SerializeObject(_Config));
