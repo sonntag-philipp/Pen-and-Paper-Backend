0eb11ce05184fa27cfa2679010d536d3337d7d56
Philipp Sonntag <sonntag.philipp@outlook.de>
2017-11-18 22:41:48 +0100

    Removed the old project and initialized a new one using Grapevine for better route management and linq2sql to manage the database connections.

---
+++ Pen-and-Paper-Backend/Pen-and-Paper-Backend/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grapevine.Server;
+using System;
+using PnP_Backend.Resources;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pen_and_Paper_Backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RestServer server = new RestServer())
+            {
+                server.LogToConsole().Start();
+                Console.ReadLine();
+                server.Stop();
+            }
+        }
+    }
+}
---
+++ Pen-and-Paper-Backend/Pen-and-Paper-Backend/Resources/AccountsResource.cs
@@ -0,0 +1,28 @@
+﻿using Grapevine.Interfaces.Server;
+using Grapevine.Server.Attributes;
+using Grapevine.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PnP_Backend.Resources
+{
+    [RestResource(BasePath = "/accounts")]
+    public class AccountsResource
+    {
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/get")]
+        public IHttpContext GetAccount(IHttpContext ctx)
+        {
+            return ctx;
+        }
+
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/get")]
+        public IHttpContext GetAccountTwo(IHttpContext ctx)
+        {
+            ctx.Response.SendResponse(Encoding.UTF8.GetBytes("Antwort auf accounts/get"));
+            return ctx;
+        }
+    }
+}
